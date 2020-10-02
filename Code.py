3f3253327d8a04ba31c7fde58938e3cc7a7e2ce7
Surya Sarkar <36451932+suryasarkar2@users.noreply.github.com>
2020-10-02 21:57:35 +0530

    Update Code.py
    Cleaned the code, added useful comments and improved understandability of the overall program.

--- Code.py
+++ Code.py
@@ -17,67 +17,67 @@ import matplotlib.pyplot as plt
 #db = firebase.database()
 
 backsub = cv2.createBackgroundSubtractorMOG2()
-cap = cv2.VideoCapture("C:/Users/ASUS/Desktop/video/tr2.mp4") 
-i = j= c= 0
+cap = cv2.VideoCapture("C:/Users/ASUS/Desktop/video/tr2.mp4") #Path of the source video || Change if you want to test with your own video
+i = j = c = 0
 ret, frame = cap.read()
 im=plt.imshow(frame, interpolation='none')
 plt.ion()
-minArea=1
+minArea = 1
 while True:
     ret, frame = cap.read()
     fgmask = backsub.apply(frame, None, 0.01)  
     if fgmask is None:
         break                                    
-    erode=cv2.erode(fgmask,None,iterations=2)     
-    moments=cv2.moments(erode,True)              
-    area=moments['m00']
-    cv2.line(frame,(150,250),(297,250),(0,0,255),5)
-    cv2.line(frame,(375,250),(530,250),(0,0,255),5)
+    erode = cv2.erode(fgmask,None,iterations=2)     
+    moments = cv2.moments(erode,True)              
+    area = moments['m00']
+    cv2.line(frame, (150,250), (297,250), (0,0,255), 5)
+    cv2.line(frame, (375,250), (530,250), (0,0,255), 5)
 
-    if area >=minArea:
-        x=int(moments['m10']/moments['m00'])
-        y=int (moments['m01']/moments['m00'])
+    if area >= minArea:
+        x = int(moments['m10']/moments['m00'])
+        y = int(moments['m01']/moments['m00'])
         
-        if(c==0):
-            if x>150 and x<250 and y>249 and y<297: #range of co-ordinates for objects in left lane
-                i=i+1
-                c=8
-                cv2.line(frame,(150,250),(297,249),(0,255,0),5)
+        if(c == 0):
+            if x > 150 and x < 250 and y > 249 and y < 297: #range of co-ordinates for objects in left lane
+                i = i+1
+                c = 8
+                cv2.line(frame, (150,250), (297,249), (0,255,0), 5)
  #               data={"time": time.time(), "count_l":i}
  #               res=db.child("place").child("1234").push(data, user['idToken'])
-                print(i)
+ #               print(i)
             
-            elif x>461 and x<558 and y>299 and y<355:#range of co-ordinates for objects in right lane
-                j=j+1
-                c=8
-                cv2.line(frame,(375,250),(530,250),(0,255,0),5)
+            elif x > 461 and x < 558 and y > 299 and y < 355:#range of co-ordinates for objects in right lane
+                j = j+1
+                c = 8
+                cv2.line(frame, (375,250), (530,250), (0,255,0), 5)
  #               data={"time": time.time(), "count_r":j}
  #               res=db.child("place").child("1234").push(data, user['idToken'])
-                print(j)
+ #               print(j)
         else:
-            c=c-1  
-            continue  
+            c = c-1
+            continue
         
             
-    cv2.putText(frame,'Left Lane: %r' %i, (10,30), cv2.FONT_HERSHEY_COMPLEX,
+    cv2.putText(frame, 'Left Lane: %r' %i, (10,30), cv2.FONT_HERSHEY_COMPLEX,
                         1, (0, 255, 0), 2)
-    cv2.putText(frame,'Right Lane: %r' %j, (380,30), cv2.FONT_HERSHEY_COMPLEX,
+    cv2.putText(frame, 'Right Lane: %r' %j, (380,30), cv2.FONT_HERSHEY_COMPLEX,
                         1, (255, 0, 0), 2)
         
     key = cv2.waitKey(30)
     
-    if  key== ord('p'):
+    if  key == ord('p'):
         while True:
-            key2=cv2.waitKey(0)
-            if key2==ord('p'):
+            key2 = cv2.waitKey(0)
+            if key2 == ord('p'):
                 break
     im.set_data(frame)
     plt.pause(0.01)
     cv2.imshow("Traffic Video Feed", frame)
-    cv2.imshow("Foreground Mask", fgmask)
+    cv2.imshow("Foreground Mask", fgmask) #Eliminated static background, only the moving part of the video is considered
     if key == ord('q'):
             break
 
 plt.ioff()
 plt.show(False)
-cv2.destroyAllWindows()  
+cv2.destroyAllWindows() #close the program
